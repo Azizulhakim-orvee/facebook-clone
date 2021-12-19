45bd4f2d7ae4bdcdd936b073f18bfee7c08635af
new name <new email>
2021-12-20 03:28:04 +0600

    auth updated

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,27 +1,26 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
-
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-
   ],
+  secret: 'K7rishSXx2f284JFJABu+VIeW47I+knxQCbIIkO2q9Q=',
   pages: {
-    signIn: "/auth/signin",
+    signIn: '/auth/signin',
   },
   callbacks: {
     async session({ session, token, user }) {
       session.user.username = session.user.name
-        .split(" ")
-        .join("_")
-        .toLowerCase(); 
+        .split(' ')
+        .join('_')
+        .toLowerCase();
 
-        session.user.uid = token.sub;
-        return session;
+      session.user.uid = token.sub;
+      return session;
     },
   },
 });
